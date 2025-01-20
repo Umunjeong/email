f91f587846a5fddce082898cb1d29509ce1efbeb
qpaseo <qazcgik@gmail.com>
2025-01-20 15:50:39 +0900

    update : email text update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React from "react";
 import {
   Dev,
   Email_From_Box,
@@ -10,46 +10,10 @@ import {
 } from "./App";
 
 import Logo from "./assets/img/Img_umunjeong-Black_Logo.png";
-import { supabase } from "./db/supabase";
 
 function Email() {
-  const [currentEmail, setCurrentEmail] = useState(""); // 상태로 이메일 관리
-  const queryParams = new URLSearchParams(window.location.search);
-  const accessToken = queryParams.get("access_token");
-
-  const handleVerification = async () => {
-    if (!accessToken) {
-      alert("토큰이 존재하지 않습니다. 이메일 링크를 확인해주세요.");
-      return;
-    }
-
-    try {
-      // Supabase에서 인증 요청
-      const { data, error } = await supabase.auth.setAuth(accessToken);
-
-      if (error) {
-        alert(`인증 실패: ${error.message}`);
-        return;
-      }
-
-      const user = data.user;
-      setCurrentEmail(user.email);
-
-      // 이메일 인증 처리
-      const { error: verificationError } = await supabase.auth.verifyOtp({
-        type: "signup",
-        token: accessToken,
-      });
-
-      if (verificationError) {
-        alert(`인증 실패: ${verificationError.message}`);
-      } else {
-        alert("이메일 인증이 성공적으로 완료되었습니다.");
-      }
-    } catch (err) {
-      console.error("인증 요청 중 오류 발생:", err);
-      alert("인증 요청 중 오류가 발생했습니다.");
-    }
+  const handleRedirect = () => {
+    window.location.href = "https://umunjeong.netlify.app/"; // 버튼 클릭 시 해당 URL로 리다이렉트
   };
 
   return (
@@ -63,7 +27,6 @@ function Email() {
           <span>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;환영합니다! 회원가입의 모든 절차가
             완료되었습니다.
-            <strong>{currentEmail}</strong>
             <br />
             &nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;로그인으로
             돌아가서 다시 로그인하여 주세요
@@ -74,8 +37,8 @@ function Email() {
           </span>
         </SpenBox>
 
-        <ButtonBox onClick={() => handleVerification()}>
-          <Submit_Button>
+        <ButtonBox>
+          <Submit_Button onClick={handleRedirect}>
             <span>우문정 바로가기</span>
           </Submit_Button>
         </ButtonBox>
