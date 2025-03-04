87fe40efcc19844f3f7515a4dc9594ff7c78eaee
qpaseo <qazcgik@gmail.com>
2025-03-04 17:49:38 +0900

    Add Codecov bundler plugin

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 export default defineConfig({
   plugins: [
-    react(),
     codecovVitePlugin({
       enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
-      bundleName: "<bundle project name>",
+      bundleName: "umunjeong-email",
       uploadToken: process.env.CODECOV_TOKEN,
     }),
   ],
